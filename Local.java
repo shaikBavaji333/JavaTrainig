37ea5e595023992176c330f2f532f4b9395ed113
shaikBavaji333 <shaikbavaji333@gmail.com>
2024-10-08 12:06:37 +0530

    Local.java added

---
+++ Local.java
@@ -0,0 +1,14 @@
+class Demo{
+    public void display(){
+    String name="Bavaji";
+    int age=25;
+    System.out.println(name+" "+age);
+    }
+}
+
+public class Local {
+    public static void main(String[] args) {
+       Demo D1= new Demo();
+       D1.display();
+    }
+}
