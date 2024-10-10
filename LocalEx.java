f60f6b5ed5e6bcf8d1026e2b4ba8e706df620dfc
shaikBavaji333 <shaikbavaji333@gmail.com>
2024-10-10 18:17:18 +0530

    LocalEx.java added

---
+++ LocalEx.java
@@ -0,0 +1,14 @@
+public class LocalEx{
+    public void display(){
+
+    String name="Bavaji";
+    int age=25;
+    System.out.println(name+" "+age);
+
+}
+public static void main(String[] args){
+        LocalEx l1=new LocalEx();
+        l1.display();
+    }
+
+}
