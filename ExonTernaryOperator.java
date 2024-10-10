10188000676992690400a7e7ac7325f788abebdb
shaikBavaji333 <shaikbavaji333@gmail.com>
2024-10-10 08:02:17 +0530

    ExonTernaryOperator.java added

---
+++ ExonTernaryOperator.java
@@ -0,0 +1,8 @@
+public class ExonTernaryOperator {
+    public static void main(String[] args) {
+        int a=10,b=20;
+        int result=(a<b)?a:b;
+        System.out.println(result);
+
+    }
+}
