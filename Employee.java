ed3e56d7a940628ccd917aede37de5b68cb02319
shaikBavaji333 <shaikbavaji333@gmail.com>
2024-10-16 20:30:59 +0530

    Encapsulation

---
+++ Employee.java
@@ -0,0 +1,35 @@
+public class Employee {
+
+    //variables
+
+    private int empId;
+    private String empName;
+    private double empSalary;
+
+    //Construtor
+
+
+    public void setEmpId(int empId) {
+        this.empId = empId;
+    }
+
+    public void setEmpName(String empName) {
+        this.empName = empName;
+    }
+
+    public void setEmpSalary(double empSalary) {
+        this.empSalary = empSalary;
+    }
+
+    public int getEmpId() {
+        return empId;
+    }
+
+    public String getEmpName() {
+        return empName;
+    }
+
+    public double getEmpSalary() {
+        return empSalary;
+    }
+}
