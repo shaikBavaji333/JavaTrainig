360d12dbdd6d35465128c3e69fa9812766babd6c
shaikBavaji333 <shaikbavaji333@gmail.com>
2024-10-18 20:04:56 +0530

    Interface

---
+++ BankAccount.java
@@ -0,0 +1,8 @@
+public interface BankAccount {
+
+    public abstract void deposit(double amount);            //abstract method,declare of variables.
+    public abstract void withdraw(double amount);
+    double checkBalance();
+
+
+}
